d7dee9a49d565c92e6add469230f0fd29239a8b1
Kotova <ekaterina.kotova@zooplus.com>
2020-03-13 03:53:40 +0100

    Fix script fix maven release plugin versioning

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-RELEASE_VERSION = git describe --abbrev=0 --tags
-echo "Release tag: ${RELEASE_VERSION}"
+RELEASE_VERSION=$(git describe --abbrev=0 --tags)
+echo "Deploy version ${RELEASE_VERSION} on prod"
 
 aws elasticbeanstalk create-application-version --application-name realestate-check \
 --version-label ${RELEASE_VERSION} --source-bundle \
