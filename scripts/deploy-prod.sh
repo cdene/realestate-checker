1ad303f9fd7b4da92ce08962672888d6ef753474
Kotova <ekaterina.kotova@zooplus.com>
2020-03-13 03:20:25 +0100

    Approach to add a deploy on prod script

---
+++ scripts/deploy-prod.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+RELEASE_VERSION = git describe --abbrev=0 --tags
+
+aws elasticbeanstalk create-application-version --application-name realestate-check \
+--version-label ${RELEASE_VERSION} --source-bundle \
+S3Bucket=cdeneuve-maven-repo,\
+S3Key=release/com/cdeneuve/realestate/realestate-checker/${1}/realestate-checker-${1}.jar
+
+aws elasticbeanstalk update-environment --environment-name realestate-check-env \
+ --version-label ${RELEASE_VERSION}
