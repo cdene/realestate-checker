03c50da1465b2b760fc0f05ca5d1479c11bf2237
Kotova <ekaterina.kotova@zooplus.com>
2020-05-22 15:52:19 +0200

    Commented out deploy process

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -3,10 +3,10 @@
 RELEASE_VERSION=$(git describe --abbrev=0 --tags)
 echo "Deploy version ${RELEASE_VERSION} on prod"
 
-aws elasticbeanstalk create-application-version --application-name realestate-check \
---version-label ${RELEASE_VERSION} --source-bundle \
-S3Bucket=cdeneuve-maven-repo,\
-S3Key=release/com/cdeneuve/realestate/realestate-checker/${RELEASE_VERSION}/realestate-checker-${RELEASE_VERSION}.jar
+#aws elasticbeanstalk create-application-version --application-name realestate-check \
+#--version-label ${RELEASE_VERSION} --source-bundle \
+#S3Bucket=cdeneuve-maven-repo,\
+#S3Key=release/com/cdeneuve/realestate/realestate-checker/${RELEASE_VERSION}/realestate-checker-${RELEASE_VERSION}.jar
 
 #aws elasticbeanstalk update-environment --environment-name realestate-check-env \
 # --version-label ${RELEASE_VERSION}
