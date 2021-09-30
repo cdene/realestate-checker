ee5181d1af27ee20c323ddef412e0927bc812d05
Kate Kotova <kate.kotova@reece.com.au>
2021-09-30 10:50:55 +1000

    Updated according to new format

--- src/main/java/com/cdeneuve/realestate/core/service/JsonApartmentParser.java
+++ src/main/java/com/cdeneuve/realestate/core/service/JsonApartmentParser.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -22,35 +24,74 @@ import java.util.stream.StreamSupport;
 public class JsonApartmentParser implements ApartmentParser {
     private final ObjectMapper objectMapper;
 
-    @SneakyThrows
     @Override
     public List<Apartment> parse(String source) {
-        ArrayNode resultList = (ArrayNode) objectMapper.readTree(source).findValue("resultlistEntry");
+        Optional<ArrayNode> resultList = findResultList(source);
+        return resultList
+                .map(it -> StreamSupport.stream(resultList.get().spliterator(), false)
+                        .map(ApartmentParser::getApartment)
+                        .collect(Collectors.toList()))
+                .orElseGet(ArrayList::new);
+    }
+
+    @SneakyThrows
+    private Optional<ArrayNode> findResultList(String source) {
+        var tree = objectMapper.readTree(source);
         //drop huge string
         source = "";
+        var resultNode = tree.findValue("resultlist.resultlist");
+        if (resultNode.isEmpty()) {
+            log.warn("Result Node is empty (why?)");
+            log.warn("Here is list of keys: {}", tree.fieldNames());
+            return Optional.empty();
+        } else {
+            var resultList = resultNode.findValue("resultlistEntry");
+            if (resultList.isEmpty() || !resultList.isArray()) {
+                log.warn("Result list is array={} and is empty ={}", resultList.isArray(), resultList.isEmpty());
+                return Optional.empty();
+            } else {
+                return Optional.of((ArrayNode) resultList);
+            }
+        }
+    }
 
-        List<Apartment> apartments = StreamSupport.stream(resultList.spliterator(), false)
-                .map(entry -> {
-                    JsonNode details = entry.get("resultlist.realEstate");
-                    return Apartment.builder()
-                            .extId(entry.get("realEstateId").asText())
-                            .title(details.get("title").asText())
-                            .area(new BigDecimal(details.get("livingSpace").asText()))
-                            .price(new BigDecimal(details.path("calculatedTotalRent").path("totalRent").path("value").asText()))
-                            .rooms(new BigDecimal(details.get("numberOfRooms").asText()))
-                            .zipCode(details.path("address").path("postcode").asText())
-                            .street(details.path("address").path("street").asText())
-                            .district(details.path("address").path("quarter").asText())
-                            .coordLat(details.path("address").path("wgs84Coordinate").path("latitude").asDouble())
-                            .coordLon(details.path("address").path("wgs84Coordinate").path("longitude").asDouble())
-                            .address(details.path("address").path("description").path("text").asText())
-                            .timestamp(LocalDateTime.now(ZoneId.of("Europe/Berlin")))
-                            .info(entry.toPrettyString())
-                            .build();
-                })
-                .collect(Collectors.toList());
+    private static class ApartmentParser {
+        private static final String APARTMENT_DETAILS_NODE_NAME = "resultlist.realEstate";
+        private static final String REAL_ESTATE_ID = "realEstateId";
+        private static final String TITLE = "title";
+        private static final String LIVING_SPACE = "livingSpace";
+        private static final String CALCULATED_TOTAL_RENT = "calculatedTotalRent";
+        private static final String TOTAL_RENT = "totalRent";
+        private static final String VALUE = "value";
+        private static final String NUMBER_OF_ROOMS = "numberOfRooms";
+        private static final String ADDRESS = "address";
+        private static final String POSTCODE = "postcode";
+        private static final String STREET = "street";
+        private static final String QUARTER = "quarter";
+        private static final String COORDINATE = "wgs84Coordinate";
+        private static final String LATITUDE = "latitude";
+        private static final String LONGITUDE = "longitude";
+        private static final String DESCRIPTION = "description";
+        private static final String TEXT = "text";
+        private static final ZoneId DEFAULT_ZONE_ID = ZoneId.of("Europe/Berlin");
 
-
-        return apartments;
+        private static Apartment getApartment(JsonNode entry) {
+            JsonNode details = entry.get(APARTMENT_DETAILS_NODE_NAME);
+            return Apartment.builder()
+                    .extId(entry.get(REAL_ESTATE_ID).asText())
+                    .title(details.get(TITLE).asText())
+                    .area(new BigDecimal(details.get(LIVING_SPACE).asText()))
+                    .price(new BigDecimal(details.path(CALCULATED_TOTAL_RENT).path(TOTAL_RENT).path(VALUE).asText()))
+                    .rooms(new BigDecimal(details.get(NUMBER_OF_ROOMS).asText()))
+                    .zipCode(details.path(ADDRESS).path(POSTCODE).asText())
+                    .street(details.path(ADDRESS).path(STREET).asText())
+                    .district(details.path(ADDRESS).path(QUARTER).asText())
+                    .coordLat(details.path(ADDRESS).path(COORDINATE).path(LATITUDE).asDouble())
+                    .coordLon(details.path(ADDRESS).path(COORDINATE).path(LONGITUDE).asDouble())
+                    .address(details.path(ADDRESS).path(DESCRIPTION).path(TEXT).asText())
+                    .timestamp(LocalDateTime.now(DEFAULT_ZONE_ID))
+                    .info(entry.toPrettyString())
+                    .build();
+        }
     }
 }
--- src/main/java/com/cdeneuve/realestate/core/service/SearchRefresher.java
+++ src/main/java/com/cdeneuve/realestate/core/service/SearchRefresher.java
@@ -1,30 +1,25 @@
 package com.cdeneuve.realestate.core.service;
 
-import com.cdeneuve.realestate.core.model.*;
+import com.cdeneuve.realestate.core.model.ErrorNotification;
+import com.cdeneuve.realestate.core.model.Filter;
+import com.cdeneuve.realestate.core.model.Search;
 import com.cdeneuve.realestate.core.notification.NotificationManager;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.LinkedList;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class SearchRefresher {
     private static final String searchUrl = "https://www.immobilienscout24.de/Suche/de/bayern/muenchen/wohnung-mieten?";
-
-    private final LinkedList<RefreshAttempt> refreshAttempts = new LinkedList<>();
 
     private final RestTemplate restTemplate;
     private final SearchProcessor searchProcessor;
     private final NotificationManager notificationManager;
-
-    public SearchRefresher(RestTemplate restTemplate, SearchProcessor searchProcessor, NotificationManager notificationManager) {
-        this.restTemplate = restTemplate;
-        this.searchProcessor = searchProcessor;
-        this.notificationManager = notificationManager;
-    }
 
     public void refreshSearch(Search search) {
         try {
